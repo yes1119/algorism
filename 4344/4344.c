a54b14f4f1b908838f09a83702c809922cbde9ab
seokjin <vvviiiccc@naver.com>
2017-01-24 20:05:05 +0900

    Add files via upload

---
+++ 4344/4344.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+ int c;
+ int st[1001];
+ int n;
+
+ scanf("%d",&c);
+ for(int i=0; i<c; i++){
+  
+  scanf("%d",&n);
+  float sum=0;
+  float cnt=0;
+  for(int j=0; j<n; j++) {
+   scanf("%d", &st[j]);
+   sum +=st[j];
+  }
+  sum = sum/n;
+  for(int k=0; k<n; k++) {
+   if(sum<st[k]) {
+    cnt++;
+   }
+  }
+  float ave = (float)((cnt*100)/n);
+
+  printf("%.3f%%\n",ave);
+ }
+}
