14e39fb4e455ab3cfa3d525e25634fd9985eed0b
seokjin <vvviiiccc@naver.com>
2017-01-24 18:15:12 +0900

    Update 2839.c

--- 2839/2839.c
+++ 2839/2839.c
@@ -8,21 +8,24 @@ int main()
  while(1){
   if(N==0) break;
 
-  else if(N<0) {
+  else if(N<0) 
+  {
    cnt = -1;
    break;
   }
-  else if(N%5==0) {
+  else if(N%5==0) 
+  {
    N = N-5;
    cnt++;
   }
-  else if(N%3==0) {
+  else if(N%3==0)
+  {
    N = N-3;
    cnt++;
   }
-  //N이 5로 안나눠지고 3으로도 안나눠 질때
-  // 8,11 같은 경우 
-  else if(N%5!=0 && N%3!=0) {
+
+  else if(N%5!=0 && N%3!=0) 
+  {
    N= N-5;
    cnt++;
   }
