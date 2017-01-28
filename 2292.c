6dc9ad3e65365fdf08a689ceed7a4b9fc4cc3b65
seokjin <vvviiiccc@naver.com>
2017-01-29 05:59:17 +0900

    벌집

---
+++ 2292.c
@@ -0,0 +1,29 @@
+/*
+problem : https://www.acmicpc.net/problem/2292
+*/
+
+#include <stdio.h>
+int main(void)
+{
+	int count = 1;
+	int increment = 0;
+	int input;
+        
+    scanf("%d",&input);
+ 
+ 
+	for (int i = 2; i <= input; i++)
+	{
+		if (i== 6*increment+2)
+		{
+			increment += count;
+			count++;		
+		}
+	}
+ 
+	printf("%d", count);
+    
+    return 0;
+ 
+ 
+}
