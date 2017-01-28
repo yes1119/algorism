f40e3c078bc8519b45f6e275139aed898d6e5f8f
seokjin <vvviiiccc@naver.com>
2017-01-29 05:58:18 +0900

    한수

---
+++ 1065.c
@@ -0,0 +1,32 @@
+/*
+problem : https://www.acmicpc.net/problem/1065
+*/
+
+#include <stdio.h>
+
+int main() 
+{
+	int N,count=0;
+	int a, b, c;
+	scanf("%d", &N);
+
+	for (int i = 1; i <= N; i++)
+	{
+		if (i < 100)
+			count=i;
+		if (i >= 100 && i < 1000)
+		{
+			a = i / 100;
+			b = (i % 100) / 10;
+			c = (i % 100) % 10 / 1;
+
+			if (a - b == b - c)
+				count++;
+		}
+		
+	}
+	printf("%d", count);
+    
+    return 0;
+
+}
