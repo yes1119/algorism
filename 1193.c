073cf08140f487a32df0c78ec1a88faa214d621b
seokjin <vvviiiccc@naver.com>
2017-01-29 19:30:48 +0900

    분수찾기

---
+++ 1193.c
@@ -0,0 +1,32 @@
+/*
+problem : https://www.acmicpc.net/problem/1193
+*/
+
+#include <stdio.h>
+ 
+int main()
+{
+    int n;
+    int d;
+	int num= 0,i=1;
+ 
+    scanf("%d", &n);
+
+	while(num < n)
+	{
+		num+=i;
+		i++;
+	}
+	num-=--i;
+
+	d= n-num;
+
+
+	if ((i % 2))
+		printf("%d/%d\n", i-d+1, d);	
+    else
+		printf("%d/%d\n", d, i-d+1);
+		
+	
+    return 0;
+}
