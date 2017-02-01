b2ab658b10735bea5ff5e0eb0d223d851f25db23
seokjin <vvviiiccc@naver.com>
2017-02-02 03:54:13 +0900

    ACM호텔

---
+++ 10250.c
@@ -0,0 +1,25 @@
+/*
+problem : https://www.acmicpc.net/problem/10250
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	int t,h=0,w=0,n=0,i,j;
+
+    scanf("%d",&t);
+
+	while(t--)
+	{
+		scanf("%d %d %d",&h,&w,&n);
+
+		i= (n-1)/h;
+		j= (n-1)%h;
+
+		printf("%d%02d\n",j+1,i+1);
+	}
+
+
+	return 0 ;
+}
